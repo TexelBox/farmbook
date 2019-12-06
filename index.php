e7b5902ecddb9305d245f465d1c7e2b83589f515
Aaron Hornby (Desktop) <aaron.hornby@outlook.com>
2019-12-06 08:57:31 -0700

    removed lorem ipsum

--- index.php
+++ index.php
@@ -27,9 +27,6 @@
         <h1>Welcome</h1>
         <p>Agricultural plights have existed as long as agriculture has. Subsequently, measures like pesticides and improved farming practices have been progressively advancing with science and technology. Creating a database and an accompanying program for a large scale of farms is our solution for consolidating data for farmers, researchers, suppliers, and insurers to work together to ensure food safety.</p>
     </section>
-    <aside id="sidebar">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, repellat? Velit explicabo voluptatum cum earum totam laborum maiores illum veritatis facilis dolor delectus, quam, harum dignissimos voluptatibus nostrum amet assumenda. Voluptatem minima consectetur exercitationem distinctio quas excepturi nostrum, esse, delectus sint earum nesciunt debitis molestias mollitia, quam fuga dignissimos modi alias libero obcaecati. Perspiciatis ducimus, exercitationem delectus aliquid harum, quae suscipit enim nisi vitae explicabo magnam quas, beatae molestias. Vitae eius ullam officia! Reprehenderit rerum impedit, placeat, provident voluptatum quibusdam id pariatur quasi optio ea illo tenetur qui, soluta repellat velit eveniet nemo consequatur quis vel rem doloremque earum inventore?</p>
-    </aside>
 </div>
 
 <!--form action="add.php" method="post">
